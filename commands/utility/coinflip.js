168107f55ccddcae4096dbe4052fc60fe8379429
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-01-31 18:23:15 -0500

    Create coinflip.js

---
+++ commands/utility/coinflip.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'coinflip',
+    aliases: [],
+    category: 'core',
+    utilisation: '{prefix}coinflip',
+
+    execute(client, message) {
+        var max=1
+        var min=0
+        var coin = (Math.floor(Math.random() * (max - min + 1) + min))?'heads':'tails';
+        message.channel.send(`:coin: - Coin : **${coin}** !`);
+    },
+};
