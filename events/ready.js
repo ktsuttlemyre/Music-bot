abb596abd99df1c97f2cbe22a66c7ca8072141c8
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2021-01-31 18:10:21 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ module.exports = async (client) => {
     console.log(`Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
     client.user.setActivity(client.config.discord.activity);
-    client.player.setVolume('', 10);
+
 };
